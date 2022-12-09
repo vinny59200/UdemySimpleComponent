0d6e33bfb027a2a8ad3bd4a305e3e3e7a8216727
vvauban <vvauban@gmail.com>
2022-12-09 18:36:51 +0100

    Init

---
+++ src/HelloMessageClassVersion.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class HelloMessageClassVersion extends React.Component {
+
+    render() {
+
+        return <div>Hello {this.props.name}</div>;
+
+    }
+
+}
+
+export default HelloMessageClassVersion;
---
+++ src/HelloMessageFunctionVersion.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HelloMessageFunctionVersion(props) {
+
+    return (
+
+        <div>Hello {props.name} from fn comp</div>
+
+    );
+
+}
+
+export default HelloMessageFunctionVersion;
